c27d5eabb2398bcc034e88246f06afe6e227724e
Jefferson <jefry04@gmail.com>
2022-05-09 17:36:26 -0500

    Create folder structure

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header'
+import Sidebar from  "./Sidebar"
+
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <Sidebar />
+    </>
+  )
+}
+
+export default Layout
---
+++ src/pages/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>Formulario de Login</div>
+  )
+}
+
+export default Login
