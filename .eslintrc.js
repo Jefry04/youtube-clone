e8a8cfcfb5e167c2f3e698234d1c25991efcbc3e
Andres Zuñiga <andres.zuniga.063@gmail.com>
2022-05-10 11:18:32 -0500

    update eslint and prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,22 @@
+const RULES = {
+  OFF: 'off',
+  ERROR: 'error',
+  WARN: 'warn',
+};
+
 module.exports = {
   env: {
     browser: true,
     es2021: true,
     node: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -12,6 +24,16 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react'],
-  rules: {},
+  plugins: ['react', 'jsx-a11y', 'react-hooks', 'prettier'],
+  rules: {
+    'prettier/prettier': RULES.ERROR,
+    'arrow-body-style': RULES.OFF,
+    'react/function-component-definition': [
+      RULES.ERROR,
+      {
+        namedComponents: ['function-declaration', 'arrow-function'],
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+  },
 };
