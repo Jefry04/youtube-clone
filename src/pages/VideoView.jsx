92754e8936b699d44d77d8ecd3d2ce586b8b6e72
Jefferson <jefry04@gmail.com>
2022-05-19 16:43:44 -0500

    change name of component videoshow

--- src/pages/VideoView.jsx
+++ src/pages/VideoView.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import VideoList from '../components/VideoList';
 
-const VideoShow = () => {
+const VideoView = () => {
   return (
     <div className="video__container">
       <div className="donde va el vide">area visualizacion</div>
       <div className="comentarios">area comentarios</div>
-      <div className="videoshow__list">
+      <div className="videoView__list">
         <VideoList />
       </div>
     </div>
   );
 };
 
-export default VideoShow;
+export default VideoView;
