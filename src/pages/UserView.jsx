07621b02a46a49afffc6172aa93060565a10939f
Jefferson <jefry04@gmail.com>
2022-06-08 07:53:03 -0500

    change register with redux

--- src/pages/UserView.jsx
+++ src/pages/UserView.jsx
@@ -1,84 +1,60 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
-import { React, useState, useEffect } from 'react';
+import { React } from 'react';
 
-import axios from 'axios';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import ButtonAction from '../components/ButtonAction';
 import '../styles/pages/UserView.scss';
 import pic from '../assets/images/perfil5.png';
-import { getUerData } from '../store/reducers/User.reducer';
 
 function UserView() {
-  const [videoData, setVideoData] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const token = localStorage.getItem('token');
-  const dispatch = useDispatch();
-  const { user, error } = useSelector((state) => state.UserReducer);
-
-  useEffect(() => {
-    setLoading(true);
-    dispatch(getUerData(token));
-    setLoading(false);
-  }, [token, dispatch]);
-
-  if (loading === true) {
-    return (
-      <div className="page__headerview">
-        <h2> Loding...</h2>
-      </div>
-    );
-  }
+  const { user } = useSelector((state) => state.AuthReducer);
 
   return (
-    <div className="page">
-      <div className="page__headerview">
-        <div className="header__container">
-          <div className="container__profile">
-            <div className="profile__img">
-              <img src={pic} alt="Perfil image" />
-            </div>
-            <div className="profile__info">
-              <div className="info__data">
-                <div className="data__user">
-                  <p>{videoData.name}</p>
-                </div>
-                <div className="data__subscriber">
-                  <p>200 suscriptores</p>
+    user && (
+      <div className="page">
+        <div className="page__headerview">
+          <div className="header__container">
+            <div className="container__profile">
+              <div className="profile__img">
+                <img src={pic} alt="Perfil image" />
+              </div>
+              <div className="profile__info">
+                <div className="info__data">
+                  <div className="data__user">
+                    <p>video name</p>
+                  </div>
+                  <div className="data__subscriber">
+                    <p>200 suscriptores</p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="container__button">
-            <ButtonAction
-              className="btn-action--videoup"
-              content="SUBIR VIDEO"
-            />
+            <div className="container__button">
+              <ButtonAction
+                className="btn-action--videoup"
+                content="SUBIR VIDEO"
+              />
+            </div>
           </div>
         </div>
-      </div>
-      <div className="header__tabsContainer">
-        <ButtonAction className="btn-action--tabsContainer" content="INICIO" />
-        <ButtonAction className="btn-action--tabsContainer" content="VIDEOS" />
-      </div>
-      <div className="page__grillaVideos">
-        <div className="grillaVideos__videosperfil">
-          Perfil de: {user.firstName}
-          {/* <iframe
-            src={`https://www.youtube.com/embed/${videoData.videoSrc}`}
-            title="video"
-            width="100%"
-            height="100%"
+        <div className="header__tabsContainer">
+          <ButtonAction
+            className="btn-action--tabsContainer"
+            content="INICIO"
           />
-          <iframe
-            src="https://www.youtube.com/embed/afDXVnDnBf4"
-            title="video"
-            width="100%"
-            height="100%"
-          /> */}
+          <ButtonAction
+            className="btn-action--tabsContainer"
+            content="VIDEOS"
+          />
+        </div>
+        <div className="page__grillaVideos">
+          <div className="grillaVideos__videosperfil">
+            Perfil de: {user.name}
+          </div>
         </div>
       </div>
-    </div>
+    )
   );
 }
 
--- src/store/index.js
+++ src/store/index.js
@@ -2,22 +2,22 @@ import {
   legacy_createStore as createStore,
   combineReducers,
   applyMiddleware,
+  compose,
 } from 'redux';
 import thunk from 'redux-thunk';
-import AuthReducer, { userIsAuth } from './reducers/Auth.reducer';
+import AuthReducer from './reducers/Auth.reducer';
 import LayoutReducer from './reducers/Layout.reducer';
 import ModalsReducer from './reducers/Modals.reducer';
-import UserReducer from './reducers/User.reducer';
 
 const rootReducer = combineReducers({
   AuthReducer,
   LayoutReducer,
   ModalsReducer,
-  UserReducer,
 });
 
 const middleware = applyMiddleware(thunk);
 
-const store = createStore(rootReducer, middleware);
-userIsAuth()(store.dispatch);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(middleware));
 export default store;
