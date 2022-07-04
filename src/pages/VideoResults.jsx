2916c4095b13dea22bce51cda27a34aa9220aa49
Jefferson <jefry04@gmail.com>
2022-07-03 19:50:24 -0500

    fix search when doesnt find video

--- src/pages/VideoResults.jsx
+++ src/pages/VideoResults.jsx
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
 import Card from '../components/Card';
+import SkeletonElement from '../components/SkeletonElement';
 import {
   fetchFilterVideos,
   resetState,
@@ -12,7 +13,7 @@ const VideoResults = () => {
   const location = useLocation();
   const searchInput = location.state;
 
-  const { filtersVideo } = useSelector((state) => state.VideoReducer);
+  const { filtersVideo, loading } = useSelector((state) => state.VideoReducer);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -23,20 +24,31 @@ const VideoResults = () => {
     };
   }, [searchInput, dispatch]);
 
-  return filtersVideo.length > 0 ? (
-    <div className="card__container">
-      {filtersVideo.map((filtervideo) => (
-        <Card
-          key={filtervideo._id}
-          video={filtervideo}
-          className="videofilter__container"
-          isSearch
-        />
-      ))}
-    </div>
-  ) : (
-    <div className="card__container">
-      No se encontraron videos con ete criterio de: {searchInput}
+  return (
+    <div>
+      {loading && (
+        <div className="card__container">
+          {[...Array(12).keys()].map((n) => (
+            <SkeletonElement key={n} />
+          ))}
+        </div>
+      )}
+      {filtersVideo ? (
+        <div className="card__container">
+          {filtersVideo.map((filtervideo) => (
+            <Card
+              key={filtervideo._id}
+              video={filtervideo}
+              className="videofilter__container"
+              isSearch
+            />
+          ))}
+        </div>
+      ) : (
+        <div className="card__container">
+          No se encontraron videos de: {searchInput}
+        </div>
+      )}
     </div>
   );
 };
